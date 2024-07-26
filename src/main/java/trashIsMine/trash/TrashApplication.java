3d37578ae2f3b9cd8e1ca9ddc27ecf8b2c2ca9dc
GongMinGi <komki@naver.com>
2024-07-26 15:25:08 +0900

    백엔드 초기 상태

---
+++ src/main/java/trashIsMine/trash/TrashApplication.java
@@ -0,0 +1,13 @@
+package trashIsMine.trash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trashIsMine/trash/TrashApplicationTests.java
@@ -0,0 +1,13 @@
+package trashIsMine.trash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
