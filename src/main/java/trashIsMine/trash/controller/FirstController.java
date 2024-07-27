bbfb780a552633c6516020acde76912529cf792c
GongMinGi <komki@naver.com>
2024-07-28 02:48:59 +0900

    swagger 설정중... 아직 안됌

--- src/main/java/trashIsMine/trash/controller/FirstController.java
+++ src/main/java/trashIsMine/trash/controller/FirstController.java
@@ -2,7 +2,9 @@ package trashIsMine.trash.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class FirstController {
 
     @GetMapping("/hi")
