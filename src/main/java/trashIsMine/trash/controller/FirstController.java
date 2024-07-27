c3b697adb9bd52d4331d2f3d39dbb5ff8d316762
GongMinGi <komki@naver.com>
2024-07-28 02:15:58 +0900

    mySQL 관련 환경설정

---
+++ src/main/java/trashIsMine/trash/controller/FirstController.java
@@ -0,0 +1,21 @@
+package trashIsMine.trash.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        //model 객체가 "홍팍" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "hongpark");
+        return "greetings";
+    }
+
+
+    @GetMapping("/bye")
+    public String SeeYouNextTime(Model model){
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
+}
