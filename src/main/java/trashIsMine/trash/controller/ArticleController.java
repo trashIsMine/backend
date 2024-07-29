79a841e9c842ecb51034c87d33ac8f1478da5521
GongMinGi <komki@naver.com>
2024-07-30 01:09:20 +0900

    게시판 CRU 까지 구현 Controller, dto, entity, repository추가

---
+++ src/main/java/trashIsMine/trash/controller/ArticleController.java
@@ -0,0 +1,91 @@
+package trashIsMine.trash.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import trashIsMine.trash.dto.ArticleForm;
+import trashIsMine.trash.entity.Article;
+import trashIsMine.trash.repository.ArticleRepository;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        log.info(article.toString());
+        // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+        return "redirect:articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기.
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1.모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        //2.모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        //3.뷰 페이지 설정하기
+        return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //뷰 페이지 설정하기.
+        return "articles/edit";
+    }
+
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        //1.DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        //2.엔티티를 DB에 저장하기
+        //2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        //2-2. 기존 데이터 값을 갱신하기
+        if(target != null) {
+            articleRepository.save(articleEntity); // 엔티티를 DB에 저장(갱신)
+        }
+
+        return "redirect:/articles/"+ articleEntity.getId();
+    }
+
+
+}
